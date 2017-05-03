ecc6742817c814d0e8077cfb9af4d8587fde778e
Ziolson <ziolson92@gmail.com>
2017-05-03 22:48:57 +0200

    Added simple REST Controller

---
+++ src/main/java/pl/ziolekmariusz/Main.java
@@ -0,0 +1,17 @@
+package pl.ziolekmariusz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by ziolson on 03.05.17.
+ */
+
+@SpringBootApplication
+public class Main {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class, args);
+    }
+
+}
---
+++ src/main/java/pl/ziolekmariusz/controller/GreetingController.java
@@ -0,0 +1,25 @@
+package pl.ziolekmariusz.controller;
+
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import pl.ziolekmariusz.model.Greeting;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+/**
+ * Created by ziolson on 03.05.17.
+ */
+
+@RestController
+public class GreetingController {
+
+    private static final String template = "Hello, %s!";
+    private final AtomicLong counter = new AtomicLong();
+
+    @RequestMapping("/greeting")
+    public Greeting greeting(@RequestParam(value = "name", defaultValue="World") String name) {
+        return new Greeting(counter.incrementAndGet(), String.format(template, name));
+    }
+
+}
