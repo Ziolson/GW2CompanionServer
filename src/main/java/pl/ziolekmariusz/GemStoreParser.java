bfd0da07778f9ae33c9cd94fc4fd7895e5913c7b
Ziolson <ziolson92@gmail.com>
2017-05-06 21:54:07 +0200

    Added a custom gemstore parser

--- src/main/java/pl/ziolekmariusz/GemStoreParser.java
+++ src/main/java/pl/ziolekmariusz/GemStoreParser.java
@@ -1,5 +1,6 @@
 package pl.ziolekmariusz;
 
+import org.jsoup.select.Elements;
 import pl.ziolekmariusz.model.Item;
 
 import java.util.List;
@@ -9,4 +10,5 @@ import java.util.List;
  */
 public interface GemStoreParser {
 
+    public List<Item> parseItems(Elements elements);
 }
--- src/main/java/pl/ziolekmariusz/model/Item.java
+++ src/main/java/pl/ziolekmariusz/model/Item.java
@@ -13,4 +13,67 @@ public class Item {
     private int quantity;
     private String dollarOrEuroPrice;
     private String poundPrice;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public int getGemCost() {
+        return gemCost;
+    }
+
+    public void setGemCost(int gemCost) {
+        this.gemCost = gemCost;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public String getDollarOrEuroPrice() {
+        return dollarOrEuroPrice;
+    }
+
+    public void setDollarOrEuroPrice(String dollarOrEuroPrice) {
+        this.dollarOrEuroPrice = dollarOrEuroPrice;
+    }
+
+    public String getPoundPrice() {
+        return poundPrice;
+    }
+
+    public void setPoundPrice(String poundPrice) {
+        this.poundPrice = poundPrice;
+    }
+
+    @Override
+    public String toString() {
+        String text = "Name: " + name + "\n" + "Url: " + url + " \n" + "Image: " + image + "\n" + "Gem Price: " + gemCost + "\n"
+                + "Quantity: " + quantity + "\n" + "Price: " + dollarOrEuroPrice + " or " + poundPrice;
+        return text;
+    }
 }
